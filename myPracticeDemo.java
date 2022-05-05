78cd5adee82aacc278b0864d01c7f61a056a0c95
badnanx <98980519+badnanx@users.noreply.github.com>
2022-05-04 17:47:58 -0700

    demo code and demo test class complete, jar files in lib

---
+++ myPracticeDemo.java
@@ -0,0 +1,5 @@
+public class myPracticeDemo {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ myPracticeDemoTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class myPracticeDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, myPracticeDemo.add(1,2)); 
+        // failing test
+    }
+}
